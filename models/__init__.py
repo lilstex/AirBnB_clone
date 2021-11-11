8258f3f528c721b831c60b9dea912e1e02de9f1e
Emmanuel <lilstex4good@gmail.com>
2021-11-11 21:49:08 +0100

    Updated the base_model and the file_storage

--- models/__init__.py
+++ models/__init__.py
@@ -2,6 +2,7 @@
 '''This file makes the models folder a python package'''
 
 from models.engine.file_storage import FileStorage
+from models.base_model import BaseModel
 
 storage = FileStorage()
 storage.reload()
