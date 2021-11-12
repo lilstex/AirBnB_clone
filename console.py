535edb3ccd013a5493f404007dfb312044722277
Emmanuel <lilstex4good@gmail.com>
2021-11-12 14:14:22 +0100

    Updated the command interpreter to have the create method

--- console.py
+++ console.py
@@ -15,6 +15,7 @@ from models.city import City
 
 class HBNBCommand(cmd.Cmd):
     prompt = '(hbnb) '
+    class_types = ['Basemodel', 'User', 'Amenity', 'Place', 'Review', 'State', 'City']
     intro = ' Welcome to AirBnB CLI!\n Type "?" or "help" to list commands \n Type "quit" or "x" or "q" to exit the CLI'
     
     def do_quit(self, input):
@@ -27,6 +28,7 @@ class HBNBCommand(cmd.Cmd):
         pass
 
     def default(self, input):
+        '''This method is called when an unknown command is sent'''
         if input == 'x' or input == 'q':
             return self.do_quit(input)
         else:
@@ -40,8 +42,23 @@ class HBNBCommand(cmd.Cmd):
 
 
     def do_create(self, args):
-        '''....................'''
-        pass
+        ''' This method creates an instance of a specified class, and stores
+            the dictionary representation of the JSON in a file
+        '''
+        command = args.split(" ")
+        if not command:
+            print('** class name missing **')
+        elif command[0] == HBNBCommand.class_types:
+            print('** class doesn\'t exist **')
+        else:
+            # Looping through the class_types available in HBNBCommand class
+            for cls in HBNBCommand.class_types:
+                # Checkig if the class_type matches the !st command argument
+                if cls == command[0]:
+                   new_instance = '{}()'.format(command[0])
+            new_instance.save()
+            print(new_instance.id)
+
 
     def do_show(self, args):
         '''.............'''
