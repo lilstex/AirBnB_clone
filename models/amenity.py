c9ec74b27f9eedfb9d64cc132f4adb6c1ab365e1
Emmanuel <lilstex4good@gmail.com>
2021-11-12 14:39:13 +0100

    Updated the other classes

--- models/amenity.py
+++ models/amenity.py
@@ -1,13 +1,4 @@
 #!/usr/bin/python3
-<<<<<<< HEAD
-'''Amenity module'''
-from models.base_model import BaseModel
-
-
-class Amenity(BaseModel):
-    '''A class Amenity that inherits'''
-    name = ''
-=======
 '''The Amenity class that inherits from the BaseModel'''
 
 from models.base_model import BaseModel
@@ -16,4 +7,3 @@ class Amenity(BaseModel):
     '''Pass'''
     name = ''
 
->>>>>>> 214c008bf49cc94f1eea8702c544d14272b6eea5
--- models/review.py
+++ models/review.py
@@ -1,15 +1,4 @@
 #!/usr/bin/python3
-<<<<<<< HEAD
-'''Review module'''
-from models.base_model import BaseModel
-
-
-class Review(BaseModel):
-    '''A class Review for info'''
-    place_id = ''
-    user_id = ''
-    text = ''
-=======
 '''The Review class that inherits from the BaseModel'''
 
 from models.base_model import BaseModel
@@ -20,4 +9,3 @@ class Review(BaseModel):
     user_id = ''
     text = ''
 
->>>>>>> 214c008bf49cc94f1eea8702c544d14272b6eea5
